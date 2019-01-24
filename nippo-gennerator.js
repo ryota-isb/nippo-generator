f540d52bf611d407b2ed926570fad25ffbced530
ryota <ryota.isb@gmail.com>
2019-01-24 20:06:43 +0900

    CCを消去

--- nippo-gennerator.js
+++ nippo-gennerator.js
@@ -205,7 +205,7 @@
 
 				/*宛先・CCを設定*/
 				var to = 'sato@ga-design.jp'
-				var cc = 'fukuda@ga-design.jp,nishiuchi@ga-design.jp,isobe@ga-design.jp'
+				var cc = ''
 
 				/*タイトルを設定*/
 				var subject = "【" + userName + "】業務報告" + "【" + reportDate() + '】';
